0a3d5decb133a00f7155fd1645a754ad5ee00af7
wagneralmandes <wagner_almandes@hotmail.com>
2025-01-23 22:46:17 -0400

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".faq-question").forEach((button) => {
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+  });
+});
